f31c69dc20e36020668c8603ba7627772ec42666
Sebastian Pulido <sebastianpulido12p@gmail.com>
2026-08-27 14:57:23 -0500

    Excluir archivo .env

--- config/Database.php
+++ config/Database.php
@@ -3,34 +3,16 @@
 class Database
 {
     private $host = "localhost";
-    private $db = "ropastile";
-    private $user = "root";
+    private $port = "3306";
+    private $dbname= "ropastile";
+    private $username = "root";
     private $password = "";
-    private $pdo;
 
-    public function connect()
-    {
-        try {
-            $this->pdo = new PDO(
-                "mysql:host={$this->host};dbname={$this->db};charset=utf8mb4",
-                $this->user,
-                $this->password
-            );
-
-            $this->pdo->setAttribute(
-                PDO::ATTR_ERRMODE,
-                PDO::ERRMODE_EXCEPTION
-            );
-
-            $this->pdo->setAttribute(
-                PDO::ATTR_DEFAULT_FETCH_MODE,
-                PDO::FETCH_ASSOC
-            );
-
-            return $this->pdo;
-
-        } catch (PDOException $e) {
-            die("Error de conexión a la base de datos: " . $e->getMessage());
-        }
+    public function conectar () {
+        return new PDO(
+            "mysql:host={$this->host};dbname={$this->dbname}",
+            $this->username,
+            $this->password
+        );
     }
-}
+}   
--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 require_once "../config/Database.php";
 
 $database = new Database();
-$conexion = $database->connect();
+
+$pdo = $database->conectar();
 
 echo "Conexión exitosa a la base de datos ROPASTILE";
