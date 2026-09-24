f5fac912782ad83c6559a30689d98f532b0fc073
Sebastian Pulido <sebastianpulido12p@gmail.com>
2026-09-24 17:42:42 -0500

    creacion de formularios

---
+++ app/views/clientes/crear.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Crear Cliente</title>
+</head>
+<body>
+
+<h1>Crear Cliente</h1>
+
+<form action="" method="POST">
+
+    <label>Nombre:</label>
+    <input type="text" name="nombre" required>
+
+    <br><br>
+
+    <label>Apellido:</label>
+    <input type="text" name="apellido" required>
+
+    <br><br>
+
+    <label>Documento:</label>
+    <input type="text" name="documento" required>
+
+    <br><br>
+
+    <label>Teléfono:</label>
+    <input type="text" name="telefono" required>
+
+    <br><br>
+
+    <label>Correo:</label>
+    <input type="email" name="correo" required>
+
+    <br><br>
+
+    <label>Ciudad:</label>
+    <input type="text" name="ciudad" required>
+
+    <br><br>
+
+    <button type="submit">Guardar Cliente</button>
+
+</form>
+
+</body>
+</html>
